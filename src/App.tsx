40d4374126c415c91b5a1d24c5c321e9a7bc05ae
rudrasgithub <rudramanaidu99@gmail.com>
2024-07-16 12:55:46 +0530

    adjusted height

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="flex justify-center items-center bg-[#f1d4b3] min-h-screen flex-col">
       <BackgroundHeading/>
-      <main className="relative w-[972px] h-[636px] bg-white rounded-[8px] shadow-[0_4px_4px_rgba(0,0,0,0.08])]
+      <main className="relative w-[872px] h-[536px] bg-white rounded-[8px] shadow-[0_4px_4px_rgba(0,0,0,0.08])]
        grid grid-cols-[7fr_4fr] grid-rows-[59px_1fr] overflow-hidden">
         <Header/>
         <TodoList/>
